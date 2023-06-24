58c4b482d18e8d46b64f034aeb22110e5384e6e2
OBriqa <omar.briqa@estudiantat.upc.edu>
2023-06-24 22:01:19 +0200

    add routes_managers to app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from routes.route import routes_user, routes_pacients, routes_meds, routes_mqtt, routes_cotxes, routes_drones, routes_routes, routes_orders, routes_paypal
+from routes.route import routes_user, routes_pacients, routes_meds, routes_mqtt, routes_cotxes, routes_drones, routes_routes, routes_orders, routes_paypal, routes_managers
 
 app = Flask(__name__, template_folder='/opt/templates/')
 routes_cotxes(app)
@@ -11,6 +11,7 @@ routes_pacients(app)
 routes_routes(app)
 routes_user(app)
 routes_paypal(app)
+routes_managers(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
