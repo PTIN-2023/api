9d68c004b90704ca8238c96dfa472d95c4e73052
Raul Rabadan Arroyo <77171479+RabadanDotDev@users.noreply.github.com>
2023-06-25 22:28:30 +0200

    add routes_colmenes to app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from routes.route import routes_user, routes_pacients, routes_meds, routes_mqtt, routes_cotxes, routes_drones, routes_routes, routes_orders, routes_paypal, routes_managers
+from routes.route import routes_user, routes_pacients, routes_meds, routes_mqtt, routes_cotxes, routes_drones, routes_routes, routes_colmenes, routes_orders, routes_paypal, routes_managers
 
 app = Flask(__name__, template_folder='/opt/templates/')
 routes_cotxes(app)
@@ -12,6 +12,7 @@ routes_routes(app)
 routes_user(app)
 routes_paypal(app)
 routes_managers(app)
+routes_colmenes(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
