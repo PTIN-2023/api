39203dc921d69cd3ffd8b473c1663f3ad4235c8e
OBriqa <omar.briqa@estudiantat.upc.edu>
2023-06-26 23:26:38 +0200

    update beehives_global

--- controllers/controller_colmenes.py
+++ controllers/controller_colmenes.py
@@ -18,12 +18,14 @@ def beehives_global():
 
     if value['valid'] == OK:
         colmenitas = colmenas.find()
-        response['beehives'] = [{
-            'id_beehive': doc['id_beehive'],
-            'latitude': doc['location_end']['latitude'],
-            'longitude': doc['location_end']['longitude']
-            # 'url_beehive': url,
-        }for doc in colmenitas]
+        beehives = []
+        for colmena in colmenitas:
+            beehives.append({
+                'id_beehive'    : colmena['id_beehive'],
+                'latitude'      : colmena['location_end']['latitude'],
+                'longitude'     : colmena['location_end']['longitude'],
+            })
+        response['beehives'] = beehives
     else:
         response = value 
 
