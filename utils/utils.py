cd43ef399f1de7ccae05d46fbf3fd70e2a6799f7
jaume-pm <jaumepetit2002@gmail.com>
2023-06-26 20:02:52 +0200

    get closest beehive at user registration

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from flask import request
 from models.models import *
 from geopy.geocoders import Nominatim
+from math import radians, cos, sin, asin, sqrt
 
 def checktoken(token):
     if(token==""):
@@ -63,4 +64,57 @@ def get_coordinates(address):
         return None
     else:
         return location.latitude, location.longitude
+
+def distance(lat1, lat2, lon1, lon2):
+     
+    # The math module contains a function named
+    # radians which converts from degrees to radians.
+    lon1 = radians(lon1)
+    lon2 = radians(lon2)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+      
+    # Haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+ 
+    c = 2 * asin(sqrt(a))
+    
+    # Radius of earth in kilometers. Use 3956 for miles
+    r = 6371
+      
+    # calculate the result
+    return(c * r)
+
+def get_closest_beehive(city, user_latitude, user_longitude):
+    query = {"city": city}
+    result = colmenas.find(query)
+    colmenas = list(result)
+    count = len(colmenas)
+
+    if count == 1:
+        colmena = colmenas[0]
+        colmena_latitude = colmena['location_end']['latitude']
+        colmena_longitude = colmena['location_end']['longitude']
+        
+        return (colmena_latitude, colmena_longitude)
+    
+    elif count > 1:
+        closest_distance = float('inf')
+        closest_colmena = None
+        
+        for colmena in colmenas:
+            colmena_latitude = colmena['location_end']['latitude']
+            colmena_longitude = colmena['location_end']['longitude']
+            
+            distance = distance(user_latitude, colmena_latitude, user_longitude, colmena_longitude)
+            
+            if distance < closest_distance:
+                closest_distance = distance
+                closest_colmena = (colmena_latitude, colmena_longitude)
+        
+        if closest_colmena:
+            return closest_colmena
+
 
