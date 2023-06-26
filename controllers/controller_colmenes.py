b5774b50e14920f570a9edd0dd996bd886464798
OBriqa <omar.briqa@estudiantat.upc.edu>
2023-06-26 23:12:31 +0200

    update beehives_global

--- controllers/controller_colmenes.py
+++ controllers/controller_colmenes.py
@@ -4,25 +4,30 @@ from utils.utils import checktoken
 import json
 
 OK = 'ok'
-NOT_AVAILABLE_AT_CLOUD = { 'result': 'error, funcio no disponible al cloud' }
+NOT_AVAILABLE_AT_CLOUD  = { 'result': 'error, funcio no disponible al cloud' }
+NOT_AVAILABLE_AT_EDGE   = { 'result': "error, funcio no disponible a l'edge" }
 
 def beehives_global():
+
+    if is_local == 1:
+        return jsonify(NOT_AVAILABLE_AT_EDGE)
+
     data = request.get_json()
     value = checktoken(data['session_token'])
-    if value['valid'] == 'ok':
-        if is_local == 1:
-            data['session_token'] = 'internal'
-            url = cloud_api+"/api/beehives_global"
-            return requests.post(url, json=data).json()
+    response = { 'value' : value['valid'] }
+
+    if value['valid'] == OK:
         colmenitas = colmenas.find()
-        response['beehives'] = [{
-            'id_beehive': doc['id_beehive'],
-            'latitude': doc['location_end']['latitude'],
-            'longitude': doc['location_end']['longitude'],
-        }for doc in colmenitas]
+        beehives = []
+        for colmena in colmenitas:
+            beehives.append({
+                'id_beehive'    : colmena['id_beehive'],
+                'latitude'      : colmena['location_end']['latitude'],
+                'longitude'     : colmena['location_end']['longitude'],
+            })
+        response['beehives'] = beehives
     else:
-        response = value
-    return jsonify(response)    
+        response = value 
 
 def beehives_local():
 
