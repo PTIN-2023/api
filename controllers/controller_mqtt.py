949635a6a1680d47175f2a8ade4facb95188904d
OBriqa <omar.briqa@estudiantat.upc.edu>
2023-06-27 13:56:55 +0200

    update update_status case car unloading

--- controllers/controller_mqtt.py
+++ controllers/controller_mqtt.py
@@ -143,7 +143,7 @@ def update_status():
 
                 if edge_api != -1:
                     url = edge_api + "/api/unload_car"
-                    response = requests.post(url, data=jsonify(payloadJSON))
+                    response = requests.post(url, json=payloadJSON)
                     
                     if response.status_code == 200:
                         return jsonify(OK), 200
