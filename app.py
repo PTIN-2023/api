e3c4c2e51cf1f645d5831e59e1f9f66715e3b7e1
Robertolupu <55112202+Robertolupu@users.noreply.github.com>
2023-06-28 18:08:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from routes.route import routes_user, routes_pacients, routes_meds, routes_mqtt, routes_cotxes, routes_drones, routes_routes, routes_colmenes, routes_orders, routes_paypal, routes_managers
+from routes.route import routes_user, routes_prescriptions, routes_pacients, routes_meds, routes_mqtt, routes_cotxes, routes_drones, routes_routes, routes_colmenes, routes_orders, routes_paypal, routes_managers
 
 app = Flask(__name__, template_folder='/opt/templates/')
 routes_cotxes(app)
@@ -13,6 +13,7 @@ routes_user(app)
 routes_paypal(app)
 routes_managers(app)
 routes_colmenes(app)
+routes_prescriptions(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
