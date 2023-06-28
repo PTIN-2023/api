2b069cae9defe046fda159b0e4845a5fc06e2ab2
Almogawer <arnau.roca.grange@estudiantat.upc.edu>
2023-06-29 00:11:19 +0200

    Update controller_managers.py

--- controllers/controller_managers.py
+++ controllers/controller_managers.py
@@ -49,7 +49,7 @@ def list_all_orders():
                                 'date': order['date'],
                                 'state': order['state']
                                 }
-                    response.append(responses)
+                    responses.append(responses)
                 response = {'result': 'ok', 'orders': response, 'page': page, 'orders_per_page': orders_per_page}
                 
             else:
