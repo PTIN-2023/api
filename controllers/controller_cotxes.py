e476223ac307c41db6a851c64104e117a7a63111
RabadanDotDev <77171479+RabadanDotDev@users.noreply.github.com>
2023-07-02 17:18:55 +0200

    add debug prints

--- controllers/controller_cotxes.py
+++ controllers/controller_cotxes.py
@@ -169,9 +169,11 @@ def send_order_cars():
     value = checktoken(data['session_token'])
     
     if value['valid'] != OK or value['type'] != INTERNAL:
+        print("[send_order_cars] invalid token")
         return jsonify(FAILED)
     
     for car in data['assignations']:
+        print("[send_order_cars] updating car nº" + car['id_car'] + " with route nº " + car['route']['id_route'])
 
         id_car      = car['id_car']
         id_beehive  = car['id_beehive']
@@ -204,6 +206,7 @@ def send_order_cars():
 
 
 def send_car(id_car, route):
+    print("sending car " + id_car + " a route")
    
     client = mqtt.Client()
     client.connect("mosquitto", 1883, 60)
